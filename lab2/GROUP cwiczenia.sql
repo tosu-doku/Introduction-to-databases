c92aaf47be8975f202b5f8e56e6e125e5ac2e2f1
Michał Mąka <mmaka@student.agh.edu.pl>
2025-10-22 21:20:33 +0200

    add: lab2 w/o practice excercises

---
+++ lab2/GROUP cwiczenia.sql
@@ -0,0 +1,20 @@
+﻿-- Dla każdego zamówienia podaj jego wartość. 
+-- Posortuj wynik wg wartości zamówień (w malejęcej kolejności)
+
+
+-- Northwind - ćw 1
+-- Dla każdego zamówienia podaj jego wartość. 
+-- Posortuj wynik wg wartości zamówień (w malejęcej kolejności)
+SELECT OrderID, sum(UnitPrice * Quantity * (1-Discount)) as value
+FROM [Order Details]
+GROUP BY OrderID
+ORDER BY value DESC
+
+
+-- Zmodyfikuj zapytanie z poprzedniego punktu, 
+-- tak aby zwracało tylko pierwszych 10 wierszy
+
+SELECT TOP 10 UnitPrice * Quantity * (1-Discount) as value
+FROM [Order Details]
+ORDER BY value DESC
+
