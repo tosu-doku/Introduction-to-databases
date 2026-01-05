156ad3ce0141ba63ffa0828a738ee171b0a26f2f
Michał Mąka <mmaka@student.agh.edu.pl>
2026-01-05 21:48:03 +0100

    add: lab4

---
+++ lab4/subquery cwiczenia.sql
@@ -0,0 +1,86 @@
+﻿--Ćwiczenie 1
+--Podaj łączną wartość zamówienia o numerze 10250 (uwzględnij cenę za przesyłkę)
+
+--podproblem
+SELECT UnitPrice*Quantity*(1-Discount) as Cost
+FROM [Order Details] od
+JOIN Orders o ON o.OrderID = od.OrderID
+WHERE o.OrderID = 10250
+
+SELECT sum(Cost)+max(Freight) as CostSummary
+FROM(
+	SELECT UnitPrice*Quantity*(1-Discount) as Cost,Freight
+	FROM [Order Details] od
+	JOIN Orders o ON o.OrderID = od.OrderID
+	WHERE o.OrderID = 10250) as t
+
+
+
+
+-- Ćwiczenie 5
+--Podaj wszystkie produkty których cena jest mniejsza niż średnia cena produktu
+
+select ProductName, UnitPrice
+from products
+where Unitprice < (select avg(unitprice) from products)
+
+-- alternatywne
+with avgp as (select avg(unitprice) as avgprice from products)
+
+select ProductName, UnitPrice
+from products
+cross join avgp
+where Unitprice < avgp.avgprice
+
+-- Podaj wszystkie produkty których cena jest mniejsza niż średnia cena produktu danej kategorii
+with t2 as (select CategoryName, avg(unitprice) as avgp
+	from products p
+	join Categories c 
+	on p.categoryID = c.CategoryId
+	group by CategoryName)
+
+select ProductName, UnitPrice, avgp
+from Products p
+join Categories c
+	on c.CategoryID = p.CategoryID
+join t2 
+	on t2.CategoryName = c.CategoryName
+where UnitPrice < avgp
+
+
+--Dla każdego produktu podaj jego nazwę, cenę, średnią cenę wszystkich produktów 
+-- oraz różnicę między ceną produktu a średnią ceną wszystkich produktów
+
+with avgp as (select avg(unitprice) as avgprice from products)
+
+select ProductName, UnitPrice, UnitPrice-avgprice as odchylka
+from products
+cross join avgp
+
+-- Dla każdego produktu podaj jego nazwę kategorii, nazwę produktu, cenę, 
+-- średnią cenę wszystkich produktów danej kategorii oraz różnicę między ceną produktu 
+-- a średnią ceną wszystkich produktów danej kategorii
+with t2 as (select CategoryName, avg(unitprice) as avgp
+	from products p
+	join Categories c 
+	on p.categoryID = c.CategoryId
+	group by CategoryName)
+
+select ProductName, UnitPrice, avgp, UnitPrice-avgp
+from Products p
+join Categories c
+	on c.CategoryID = p.CategoryID
+join t2 
+	on t2.CategoryName = c.CategoryName
+where UnitPrice < avgp
+
+-- Ćwiczenie 6
+--Podaj produkty kupowane przez więcej niż jednego klienta
+
+select p.ProductID, COUNT(CustomerID)
+from Products p 
+join [Order Details] od
+	on p.ProductID = od.ProductID
+join Orders o
+	on o.orderid = od.orderid
+group by p.ProductID
---
+++ lab4/subquery.sql
@@ -0,0 +1,99 @@
+﻿-- Subquery
+select t.orderid, t.customerid  
+ from (select orderid, customerid  
+         from orders) as t
+
+
+-- znajdź średnią cene
+select productname, unitprice  
+     , (select avg(unitprice) from products) as average  
+from products;
+
+-- odchylenie od średniej ceny
+select productname, unitprice  
+     , (select avg(unitprice) from products) as average  
+     , unitprice - (select avg(unitprice) from products) as diff  
+from products; 
+
+
+
+select productname, unitprice  
+     , (select avg(unitprice) from products) as average  
+     , unitprice - (select avg(unitprice) from products) as diff  
+from products  
+where unitprice > (select avg(unitprice) from products)
+
+
+select *, unitPrice - average as diff  
+from  
+  (select productname, unitprice  
+     , (select avg(unitprice) from products) as average  
+   from products) t  
+where unitPrice > average;
+
+-- MySql
+set @sr = (select avg(unitprice) from products)
+select productname, unitprice,
+@sr as average, 
+UnitPrice - @sr diff
+from products
+where Unitprice > @sr
+
+
+select productname, p.categoryid, unitprice, UnitPrice - average diff
+from Products p
+    join
+        (select categoryid, avg(unitprice) average
+        from products
+        group by categoryid) cav
+        on p.CategoryID = cav.CategoryID
+where UnitPrice > average;
+
+-- alternatywne rozwiazanie
+with cav as
+(
+    select categoryid, avg(unitprice) average
+    from products
+    group by categoryID
+)
+select productname, p.categoryid, UnitPrice, UnitPrice - average diff
+from products p
+    join cav
+    on p.CategoryID = cav.CategoryID
+where UnitPrice > average
+
+
+select distinct lastname, e.employeeid
+from orders o
+right join Employees e
+on o.EmployeeID = e.EmployeeID
+and o.OrderDate = '1997-09-05'
+where OrderID is null
+
+-- alternatywne do wyzej
+select lastname, employeeid
+from Employees e
+except  -- przez zrobionie przypadku przeciwnego i zaprzeczenie
+select lastname, Employeeid
+from Employees e
+where EmployeeID in (select EmployeeID from orders o
+    where o.OrderDate = '1997-09-05')
+
+
+select lastname, employeeid  
+ from employees as e  
+ where exists (select * from orders as o  
+                where e.employeeid = o.employeeid  
+                 and  o.orderdate = '1997-09-05')
+
+-- alternatywne, zamiast exists - join
+select distinct lastname, e.employeeid  
+ from orders as o  
+ inner join employees as e  
+  on o.employeeid = e.employeeid  
+ where o.orderdate = '1997-09-05'
+
+SELECT @@VERSION
+
+
+
